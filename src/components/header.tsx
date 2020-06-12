4d6756629343dd20ffc3358c7c9079d3eaa3df94
mbournehalley <mbournehalley@gmail.com>
2020-06-11 22:19:56 -0700

    fix: header components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'gatsby'
 import React from 'react'
 
 interface Props {
@@ -6,31 +5,20 @@ interface Props {
 }
 
 const Header: React.FC<Props> = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header style={{ marginBottom: `1.45rem` }}>
+    <nav className="bp3-navbar bp3-dark">
+      <div style={{ margin: `0 auto`, width: `960px` }}>
+        <div className="bp3-navbar-group bp3-align-left">
+          <div className="bp3-navbar-heading">{siteTitle}</div>
+        </div>
+        <div className="bp3-navbar-group bp3-align-right">
+          <button className="bp3-button bp3-minimal bp3-icon-home">Home</button>
+          <button className="bp3-button bp3-minimal bp3-icon-document">Files</button>
+          <span className="bp3-navbar-divider"></span>
+          <button className="bp3-button bp3-minimal bp3-icon-git-repo"></button>
+        </div>
+      </div>
+    </nav>
   </header>
 )
 
